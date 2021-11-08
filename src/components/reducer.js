7b44ac54099e5375446a6e10da1253e1e868f1b4
Praveen Kumar <soysuperhuman@gmail.com>
2021-11-08 15:12:47 +0530

    /

--- src/components/reducer.js
+++ src/components/reducer.js
@@ -1,34 +1,16 @@
 export const initialState = {
-    basket: [
-        {
-            id:6457,
-            title:"Learning React Native: Building Native Mobile Apps...",
-            price:151,
-            rating:5,
-            image:"https://http2.mlstatic.com/D_NQ_NP_835599-MLA26239270219_102017-O.webp"
-        },
-        {
-            id:6457,
-            title:"Learning React Native: Building Native Mobile Apps...",
-            price:151,
-            rating:5,
-            image:"https://http2.mlstatic.com/D_NQ_NP_835599-MLA26239270219_102017-O.webp"
-        }
-    ],
-    user: null,
+    basket: []
 };
 
 function reducer(state, action) {
     switch(action.type) {
         case 'ADD_TO_BASKET':
-            //logic to add item to the basket
+            
             return { ...state, 
                 basket: [...state.basket, action.item],
             };
             
         case 'REMOVE_FROM_BASKET':
-            //logic for removing item from basket
-
             //cloned the basket
             let newBasket = [...state.basket];
             const index = state.basket.findIndex((basketItem) => basketItem.id === action.id);
